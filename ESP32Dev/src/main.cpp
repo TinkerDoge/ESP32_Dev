c24e5e512802cf4c5a3009e465841a15a9c8575d
TinkerDoge <95340998+TinkerDoge@users.noreply.github.com>
2024-05-13 15:06:30 +0700

    FirstTest

---
+++ ESP32Dev/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("Hello From The Setup");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Hello from Loop");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+
+}
